75ccc4df532e9d1b49318af439516f89c96c0939
Ed Summers <ehs@pobox.com>
2021-08-23 10:48:43 -0400

    yarn upgrade

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,9 @@ module.exports = {
     '@babel/preset-react',
   ],
   plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-decorators', { legacy: true, loose: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ["@babel/plugin-proposal-private-methods", { "loose": true }],
+    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
   ],
 };
